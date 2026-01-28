bf305451d893153d263261fb378651e5442c0034
greentea2417 <greentea2417@users.noreply.github.com>
2026-01-28 13:26:59 +0900

    chore: add naver site verification meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,14 +9,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko" suppressHydrationWarning>
+      <head>
+        <meta
+          name="naver-site-verification"
+          content="1a5acd7bca43d938b1312e19c2fb3677332e3a02"
+        />
+      </head>
+
       <body className="bg-gray-50 text-gray-900">
-        {/* ⭐ 홈으로 가기 링크 */}
+        {/* 홈으로 가기 링크 */}
         <HomeLink />
         {children}
         <ToastHost />
-        
       </body>
     </html>
   );
 }
-
